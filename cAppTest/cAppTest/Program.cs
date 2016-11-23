c20ba8fa2a5dacfec97457cb98f86d7bd1dd538d
Chris Lainez <lord.lainez@gmail.com>
2016-11-23 15:19:54 -0600

    Prueba Con Galo

--- cAppTest/cAppTest/Program.cs
+++ cAppTest/cAppTest/Program.cs
@@ -10,7 +10,7 @@ namespace cAppTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Nuevo Mundo 2!!!");
+            Console.WriteLine("Hola Nuevo Mundo: Con Galo!!!");
             Console.Read();
         }
     }
